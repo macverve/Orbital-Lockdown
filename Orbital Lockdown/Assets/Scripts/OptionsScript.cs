19818856e3904527ea963e085445176fd0b926d5
Zahir Mohamed <only1zahir@gmail.com>
2026-03-31 23:06:29 +0200

    Initial Settings Commit
    Added sound and music volume, audio mixer for main, linked to script.

--- Orbital Lockdown/Assets/Scripts/OptionsScript.cs
+++ Orbital Lockdown/Assets/Scripts/OptionsScript.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class OptionsScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public void BackButtonClick()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
     }
 }
