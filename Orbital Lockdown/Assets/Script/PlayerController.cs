c30eb408c1eac7eb1689c5f16a31951855f3a838
kolabor11 <pkolabor11@gmail.com>
2026-04-12 18:22:23 +0200

    Player Controls
    -Imported and cleaned up the Unity Starter Assets model prefab to serve as the base character
    -Imported a suite of motion-captured animations (Idle, Walk, Run, Strafe, and T-Pose) configured for a Humanoid rig
    -Created an advanced Animator Controller using a 2D Blend Tree to smoothly interpolate between movement animations based on X and Y velocity
    -Replaced the standard Character Controller with a Rigidbody and Capsule Collider system to allow for realistic physics and external force interactions
    -Implemented a movement script that applies forces to the Rigidbody based on target speeds (2m/s for walking, 6m/s for running)
    -Attached the main camera to the character's "Eye" bone for a true full-body perspective
    -I implemented a modular C# architecture consisting of an Input Manager and a Player Controller. The Input Manager utilizes Unity’s New Input System with event-driven callbacks. The Player Controller handles Rigidbody-based physics, procedural camera positioning tied to the character's skeletal hierarchy, and a 2D Blend Tree for fluid, directional animations."
    -Developed a camera script with mouse sensitivity controls and vertical rotation clamping to prevent 360-degree flippin
    -Added code-based smoothing (Lerping) to the animator parameters to ensure smooth transitions between movement states
    -Added a utility to lock and hide the hardware cursor during gameplay

---
+++ Orbital Lockdown/Assets/Script/PlayerController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OrbitalLockdown.Manager;
+
+namespace OrbitalLockdown.PlayerControl
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float AnimblendSpeed = 8.9f;
+
+        [SerializeField] private Transform CameraRoot;
+
+        [SerializeField] private Transform Camera;
+
+        [SerializeField] private float UpperLimit = -40f;
+
+        [SerializeField] private float BottomLimit = 70f;
+
+        [SerializeField] private float MosueSensitivity = 21.9f;
+
+        private Rigidbody _playerRigidbody;
+
+        private InputManager _inputManager;
+
+        private Animator _animator;
+
+        private bool _hasAnimator;
+
+        private int _xVelHash;
+
+        private int _yVelHash;
+
+        private float _xRotation;
+
+        private const float _walkSpeed = 2f;
+
+        private const float _runSpeed = 6f;
+
+        private Vector2 _currentVelocity;
+
+        public void Start()
+        {
+            _hasAnimator = TryGetComponent<Animator>(out _animator);
+            _playerRigidbody = GetComponent<Rigidbody>();
+            _inputManager = GetComponent<InputManager>();
+
+
+            _xVelHash = Animator.StringToHash("X_Velocity");
+            _yVelHash = Animator.StringToHash("Y_Velocity");
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void LateUpdate()
+        {
+            CamMovements();
+        }
+
+        private void Move()
+        {
+            if (!_hasAnimator) return;
+
+            float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
+
+            if (_inputManager.Move == Vector2.zero) targetSpeed = 0.1f;
+
+            _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed, AnimblendSpeed * Time.fixedDeltaTime);
+            _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed, AnimblendSpeed * Time.fixedDeltaTime);
+
+            var xVelDifference = _currentVelocity.x - _playerRigidbody.linearVelocity.x;
+            var zVelDifference = _currentVelocity.y - _playerRigidbody.linearVelocity.z;
+            _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0, zVelDifference)), ForceMode.VelocityChange);
+
+            _animator.SetFloat(_xVelHash, _currentVelocity.x);
+            _animator.SetFloat(_yVelHash, _currentVelocity.y);
+        }
+
+        private void CamMovements()
+        {
+            if(!_hasAnimator) return;
+
+            var Mouse_X = _inputManager.Look.x;
+            var Mouse_Y = _inputManager.Look.y;
+            Camera.position = CameraRoot.position;
+
+           _xRotation -= Mouse_Y * MosueSensitivity * Time.deltaTime;   
+
+            _xRotation = Mathf.Clamp(_xRotation, UpperLimit, BottomLimit);
+            Camera.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+            transform.Rotate(Vector3.up, Mouse_X * MosueSensitivity * Time.deltaTime);
+
+        }
+    }
+}
